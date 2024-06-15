be38e2bc54e4aef7d4ea7371ef3abf1ed94dfe68
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 14:19:27 +0530

    Update and rename Week-10.5 to Week-10.5.py

--- Week-10/Week-10.5.py
+++ Week-10/Week-10.5.py
@@ -1,3 +1,18 @@
+Frequency of Elements
+To find the frequency of numbers in a list and display in sorted order.
+Constraints: 
+1<=n, arr[i]<=100 
+Input: 
+1 68 79 4 90 68 1 4 5 
+output:
+ 1 2
+ 4 2
+ 5 1
+ 68 2
+ 79 1 
+90 1
+
+
 
 def count_frequency(arr):
     frequency = {}
