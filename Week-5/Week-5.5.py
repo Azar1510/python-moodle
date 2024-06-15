a7d6dd1a24cf55aa54dd99a9256d73be361b11c3
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 13:49:53 +0530

    Update and rename Week-5.5 to Week-5.5.py

--- Week-5/Week-5.5.py
+++ Week-5/Week-5.5.py
@@ -1,3 +1,25 @@
+Element Insertion
+Consider a program to insert an element / item in the sorted array. Complete the logic by filling up required code in editable section. Consider an array of size 10. The eleventh item is the data is to be inserted.
+
+
+Sample Test Cases
+Test Case 1
+Input
+1
+3
+4
+5
+6
+7
+8
+9
+10
+11
+2
+
+Output
+ITEM to be inserted:2
+
 def insert_sorted(list, n):
     list.append(n)
     sorted_list = sorted(list)
