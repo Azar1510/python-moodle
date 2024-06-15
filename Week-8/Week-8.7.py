c05a78ec59f6a925ef87052e8617a16d4abb1109
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 14:11:38 +0530

    Update and rename Week-8.7 to Week-8.7.py

--- Week-8/Week-8.7.py
+++ Week-8/Week-8.7.py
@@ -1,3 +1,9 @@
+Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
+In the American keyboard:
+⦁	the first row consists of the characters "qwertyuiop",
+⦁	the second row consists of the characters "asdfghjkl", and
+⦁	the third row consists of the characters "zxcvbnm"
+
 def findWords(words):
     row1 = set('qwertyuiop')
     row2 = set('asdfghjkl')
