ebe052e204b276e03526f878ff2d2ff20d59ba4c
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 13:22:16 +0530

    Update Week-2.4.py

--- Week-2/Week-2.4.py
+++ Week-2/Week-2.4.py
@@ -1,3 +1,5 @@
+Write a python program that takes a integer between 0 and 15 as input and displays the number of '1' s in its binary form.(Hint:use python bitwise operator.
+
 a=int(input())
 n=bin(a)
 n=n.replace("0b","")
@@ -8,3 +10,9 @@ for i in range(len(c)):
     if(int(c[i])==1):
         d+=1
 print(d)
+Sample Input
+3
+Sample Output:
+2
+Explanation:
+The binary representation of 3 is 011, hence there are 2 ones in it. so the output is 2.
