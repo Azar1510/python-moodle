2f1ae2e0cf4d5bb53d9e85a62ac2d56e618d431c
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 14:42:58 +0530

    Create Week-7.3.py

---
+++ Week-7/Week-7.3.py
@@ -0,0 +1,26 @@
+Check Product of Digits
+Write a code to check whether product of digits at even places is divisible by sum of digits at odd place of a positive integer.
+Input Format:
+
+Take an input integer from stdin.
+
+Output Format:
+
+Print TRUE or FALSE.
+
+Example Input:
+
+1256
+
+Output:
+
+TRUE
+
+Example Input:
+
+1595
+
+Output:
+
+FALSE
+
