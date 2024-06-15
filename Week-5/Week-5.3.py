73c405c2ff0e2e02f25941a73a81ab3947635847
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 13:48:02 +0530

    Update and rename Week-5.3 to Week-5.3.py

--- Week-5/Week-5.3.py
+++ Week-5/Week-5.3.py
@@ -1,3 +1,23 @@
+Count Elements
+Complete the program to count frequency of each element of an array. Frequency of a particular element will be printed once.
+Sample Test Cases
+ Test Case 1
+ Input
+ 7
+23
+45
+23
+56
+45
+23
+40
+ Output
+ 23 occurs 3 times
+45 occurs 2 times
+56 occurs 1 times
+40 occurs 1 times
+ 
+
 import collections 
 def CountFrequency(arr):
 	return collections.Counter(arr)
