54b2db4d54ff93bc7b3e04dc898f9114046c5239
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 14:18:17 +0530

    Update and rename Week-10.3 to Week-10.3.py

--- Week-10/Week-10.3.py
+++ Week-10/Week-10.3.py
@@ -1,3 +1,22 @@
+Peak Element
+Given an list, find peak element in it. A peak element is an element that is greater than its neighbors.
+An element a[i] is a peak element if
+A[i-1] <= A[i] >=a[i+1] for middle elements. [0<i<n-1]
+A[i-1] <= A[i] for last element [i=n-1]
+A[i]>=A[i+1] for first element [i=0]
+Input Format
+The first line contains a single integer n , the length of A .
+The second line contains n space-separated integers,A[i].
+
+Output Format
+Print peak numbers separated by space.
+
+Sample Input
+5
+8 9 10 2 6
+Sample Output
+10 6
+
 n = int(input())
 
 # In
