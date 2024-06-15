e8b9bd6ced921caeed2e279676de639a560284ed
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 14:01:00 +0530

    Update and rename Week-6.7 to Week-6.7.py

--- Week-6/Week-6.7.py
+++ Week-6/Week-6.7.py
@@ -1,3 +1,19 @@
+Reverse a string without affecting special characters. Given a string S, containing special characters and all the alphabets, reverse the string without affecting the positions of the special characters.
+
+Input:
+A&B
+Output:
+B&A
+Explanation: As we ignore '&' and
+As we ignore '&' and then reverse, so answer is "B&A".
+
+
+For example:
+
+Input	Result
+A&x#
+x&A#
+
 def reverse_string(s):
     s = list(s)
     l, r = 0, len(s) - 1
