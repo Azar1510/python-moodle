e97324bd088d9839627a9232bf671a746c269ea3
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 13:50:41 +0530

    Update and rename Week-5.6 to Week-5.6.py

--- Week-5/Week-5.6.py
+++ Week-5/Week-5.6.py
@@ -1,3 +1,19 @@
+
+Find the Factor
+Determine the factors of a number (i.e., all positive integer values that evenly divide into a number) and then return the pth element of the list, sorted ascending. If there is no pth element, return 0.
+Constraints
+1 ≤ n ≤ 1015
+1 ≤ p ≤ 109
+The first line contains an integer n, the number to factor.
+The second line contains an integer p, the 1-based index of the factor to return.
+
+Sample Case 0
+Sample Input 0
+10
+3
+Sample Output 0
+5
+
 import sys
 import math
  
