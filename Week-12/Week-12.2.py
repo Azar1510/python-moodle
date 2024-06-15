88d0245d6668ef0cad77fd7da6680898bc36acba
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 14:25:19 +0530

    Update and rename Week-12.2 to Week-12.2.py

--- Week-12/Week-12.2.py
+++ Week-12/Week-12.2.py
@@ -1,3 +1,10 @@
+Given an integer n, print true if it is a power of four. Otherwise, print false.
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+For example:
+Input	Result
+16	True
+5	False
+
 def is_power_of_four(n):
     if n <= 0:
         return False
