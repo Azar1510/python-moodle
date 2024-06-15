38656addd6aacde4214380fd4424339bbdaf556f
Azar1510 <231001113@rajalakshmi.edu.in>
2024-06-15 13:08:51 +0530

    Update Week-1.1.py

--- week-1/Week-1.1.py
+++ week-1/Week-1.1.py
@@ -1,3 +1,5 @@
+Write a program to convert strings to an integer and float and display its type.
+
 a=int(input())
 b=float(input())
 c=round(b,1)
@@ -5,3 +7,7 @@ print(a,end=",")
 print(type(a))
 print(c,end=",")
 print(type(c))
+
+Sample Output:
+10,<class 'int'>
+10.9,<class 'float'>
